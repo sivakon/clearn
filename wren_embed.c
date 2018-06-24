126a1ab8886ef860e5c5ea213f30f5d44948a1cc
Sivaram Konanki <sivaram.konanki@p45535.intra.rakuten.co.jp>
2018-06-24 13:21:48 +0900

    wren embed complete, call wren from C

--- wren_embed.c
+++ wren_embed.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include "wren.h"
 
+void write(WrenVM* vm, const char* str) 
+{
+    printf("%s", str);
+}
 int main(int argc, char const *argv[])
 {
+    // create a config object for WrenVM
     WrenConfiguration config; 
     wrenInitConfiguration(&config);
+    config.writeFn = write;
+
+    // create VM from the config created above
     WrenVM* vm = wrenNewVM(&config);
     WrenInterpretResult result = wrenInterpret(vm, "System.print(\"I am running in a VM!\")");
-    printf("%u", result);
     wrenFreeVM(vm);
     return 0;
 }
